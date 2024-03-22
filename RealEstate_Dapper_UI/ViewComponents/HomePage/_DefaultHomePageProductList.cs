60630d0ce0c529d3ce50a8605172025905b68374
Onur Kunaç <onrkunacc@gmail.com>
2024-03-23 01:51:28 +0300

    Admin tarafında Product işlemleri devamı...

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
@@ -20,7 +20,7 @@ namespace RealEstate_Dapper_UI.ViewComponents.HomePage
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultProductDtos>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
                 //jsonDatadan gelen json paketi deserialize edio bize values değeri olarak geri verecek.
                 return View(values);
             }
