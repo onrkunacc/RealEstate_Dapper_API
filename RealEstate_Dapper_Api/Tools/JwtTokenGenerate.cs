9582925ca4f5505bf880420039715bcfb7fa4b98
Onur Kunaç <onrkunacc@gmail.com>
2024-08-09 12:05:59 +0300

    JWT Token Generate

---
+++ RealEstate_Dapper_Api/Tools/JwtTokenGenerate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RealEstate_Dapper_Api.Tools
+{
+    public class JwtTokenGenerate
+    {
+        public static TokenResponseViewModel GenerateToken(GetCheckAddUserViewModel model)
+        {
+            var claims = new List<Claim>();
+            if (!string.IsNullOrWhiteSpace(model.Role))
+                claims.Add(new Claim(ClaimTypes.Role, model.Role));
+            //Role assignment to token
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, model.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(model.UserName))
+                claims.Add(new Claim("Username", model.UserName));
+            //Username assignment to token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+            var signingCredentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+            //an algorithm works via hmacsha256
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefaults.ValidIssuer, audience: JwtTokenDefaults.ValidAudience, claims: claims,
+                notBefore: DateTime.UtcNow, expires: expireDate, signingCredentials: signingCredentials);
+
+            
+            //validity period of the token
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            return new TokenResponseViewModel(tokenHandler.WriteToken(token), expireDate);
+
+
+
+        }
+    }
+}
