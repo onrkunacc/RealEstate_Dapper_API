607939b752e6e0bbdfa4baaaf2e1e2ea43ad5b4e
Onur Kunaç <onrkunacc@gmail.com>
2024-09-12 16:07:57 +0300

    Login

---
+++ RealEstate_Dapper_Api/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.LoginDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(CreateLoginDto createLoginDto)
+        {
+            string query = "Select * From AppUser where Username=@username and Password=@password";
+            var parameters = new DynamicParameters();
+            parameters.Add("@username", createLoginDto.Username);
+            parameters.Add("@password",createLoginDto.Password);
+            
+            using(var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<CreateLoginDto>(query,parameters);
+                if (values != null)
+                {
+                    return Ok("Başarılı");
+                }
+                else 
+                { 
+                    return BadRequest("Başarısız");
+                }
+
+            }
+        }
+    }
+}
