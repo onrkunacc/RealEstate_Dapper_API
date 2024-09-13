91ff697e620b59b15af590df6a01176fcbb5ecc5
Onur Kunaç <onrkunacc@gmail.com>
2024-09-13 23:00:55 +0300

    Jwt Token Operations

--- RealEstate_Dapper_Api/Controllers/LoginController.cs
+++ RealEstate_Dapper_Api/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RealEstate_Dapper_Api.Dtos.LoginDtos;
 using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Tools;
 
 namespace RealEstate_Dapper_Api.Controllers
 {
@@ -23,6 +24,7 @@ namespace RealEstate_Dapper_Api.Controllers
         public async Task<IActionResult> SignIn(CreateLoginDto createLoginDto)
         {
             string query = "Select * From AppUser where Username=@username and Password=@password";
+            string query2 = "Select UserId From AppUser where Username=@username and Password=@password";
             var parameters = new DynamicParameters();
             parameters.Add("@username", createLoginDto.Username);
             parameters.Add("@password",createLoginDto.Password);
@@ -30,9 +32,15 @@ namespace RealEstate_Dapper_Api.Controllers
             using(var connection = _context.CreateConnection())
             {
                 var values = await connection.QueryFirstOrDefaultAsync<CreateLoginDto>(query,parameters);
+                var values2 = await connection.QueryFirstAsync<GetAppUserIdDto>(query2,parameters);
                 if (values != null)
                 {
-                    return Ok("Başarılı");
+                    GetCheckAddUserViewModel model = new GetCheckAddUserViewModel();
+                    model.UserName = values.Username;
+                    model.Id = values2.UserId;
+                    var token = JwtTokenGenerate.GenerateToken(model);
+
+                    return Ok(token);
                 }
                 else 
                 { 
